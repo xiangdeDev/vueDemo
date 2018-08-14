7378f9bd8c9f9d5e4a70308dd204c6a3661f6bc4
fuxiangde <fuxiangde@kankan.com>
2018-08-14 18:44:28 +0800

    vue

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <div>i am header</div>
+    <div>i am conter</div>
+    <div>i am footer</div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ src/components/conter/conter.vue
@@ -0,0 +1,6 @@
+<template>
+</template>
+
+<script type="text/ecmascript-6"></script>
+
+<style type="text/css"></style>
